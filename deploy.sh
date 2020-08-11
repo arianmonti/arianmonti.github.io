23f3853fa07ed2a9933b3c86bf017bfa9d4ba21c
arianmonti <a.m.ontazeri1385@gmail.com>
2020-08-11 12:40:27 +0430

    rebuilding site Tue 11 Aug 2020 12:40:27 PM +0430

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
-
-echo "Generating site"
+# Build the project. 
 hugo -t loveit
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages"
+# Add changes to git.
+git add -A
 
-echo "Pushing to github"
-git push --all
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -S -m "$msg"
 
+# Push source and build repos.
+git push origin gh-pages
+# For prevent any conflict, simply delete the branch master on remote 
+#git push origin :master
+git push --prefix=public https://www.github.com/fzerorubigd/fzerorubigd.github.io master
